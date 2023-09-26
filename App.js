cd4daa6842df9dbcc6e61ea9ab920cc8ed735042
jalyndboyd1 <jalyndboyd1@gmail.com>
2023-09-26 12:40:45 -0400

    Added Buttons

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import { Avatar } from "@mui/material";
-import profileImage from './assets/images/profile-image.JPG'
+import profileImage from "./assets/images/profile-image.JPG";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Avatar
-      src={profileImage}
-      alt="FlucoMars"
-      />
-      <View>{/* Links */}</View>
+      <Avatar src={profileImage} alt="FlucoMars" />
+      <View>
+        <Button title="Fluco Gram" />
+        <Button title="Fluco Twitter" />
+        <Button title="Fluco Tok" />
+        <Button title="Fluco Press" />
+      </View>
     </View>
   );
 }
@@ -19,6 +21,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
